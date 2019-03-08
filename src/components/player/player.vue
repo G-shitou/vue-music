974e6daf738d715c04b82958982549eb030d0a07
G-shitou <Gshitou@126.com>
2019-03-08 16:55:55 +0800

    小播放界面初始化

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="player">
-        <div class="logo"></div>
-        <div class=""></div>
+        <div class="logo">
+            <img :src="singing.img" alt="">        
+        </div>
+        <div class="lyric"></div>
+        <div class="iconfont" :class="isPlay?'icon-bofang':'icon-bofang1'"></div>
+        <div class="iconfont icon-liebiao"></div>
+        <!-- <audio :src="singing.audioSrc" autoplay></audio> -->
     </div>
 </template>
 
@@ -34,8 +39,31 @@ export default {
 .player
     width:100%
     height:1rem
+    box-shadow: 0px -0.5px 0 0 $grayColor
     position:fixed
     bottom:0
     left:0
-    background:$grayColor
+    display:-webkit-box
+    -webkit-box-align:center
+    -webkit-box-orient: horizontal
+    .logo,.iconfont
+        width:.7rem
+        height:.7rem
+        margin:0 .2rem
+    .logo
+        border-radius:.35rem
+        overflow:hidden
+        img
+            width:100%
+            height:100%
+    .iconfont
+        color:$mainColor
+        font-size:.5rem
+        display:-webkit-box
+        -webkit-box-align:center
+        -webkit-box-pack:center
+    .lyric
+        -webkit-box-flex:1
+        -moz-box-flex:1
+        height:.7rem
 </style>
--- src/store/modules/player.js
+++ src/store/modules/player.js
@@ -4,7 +4,11 @@ import {get,post} from '../../http/http'
 
 const state = {
     songs:[],
-    singing:{track_info:{title:1}},
+    singing:{
+        title:'',
+        audioSrc:'',
+        img:'https://y.gtimg.cn/music/photo_new/T001R300x300M000001BLpXF2DyJe2.jpg?max_age=2592000'
+    },
     isPlay:false,
     currentIndex:0
 }
@@ -17,16 +21,40 @@ const getters = {
 
 // actions
 const actions = {
-    
+    changeSong: ({state,commit,rootState},payload) => {
+        console.log(payload.song);
+        let params = {
+            guid:rootState.guid,
+            filename:'C400'+payload.song.track_info.mid+'.m4a',
+            songmid:payload.song.track_info.mid,
+            format:'json',
+            platform:'yqq',
+            cid:205361747
+        }
+        get(api.getVkey.url,{params}).then( res => {
+            console.log(res.data)
+            let song = {
+                vkey:res.data.data.items[0].vkey,
+                songmid:payload.song.track_info.mid,
+                lyric:payload.song.info.pop().content[0].value,
+                title:payload.song.track_info.title,
+                audioSrc:'http://ws.stream.qqmusic.qq.com/'+params.filename+'?fromtag=0&guid='+rootState.guid+'&vkey='+res.data.data.items[0].vkey,
+                img:'https://y.gtimg.cn/music/photo_new/T002R300x300M000'+payload.song.track_info.album.mid+'.jpg?max_age=2592000'
+            }
+            commit('changeSong',{song});
+        }).catch( error => {
+            console.log(error)
+        })
+    }
 }
 
 // mutations
 const mutations = {
-    changeSong: (state,payload) => {
-        state.singing = payload.song.detail.data;
-        state.songs.push(state.singing)
-        state.currentIndex += 1
-        console.log(state.singing)
+    changeSong(state,payload){
+        state.singing = payload.song;
+        state.songs.push(payload.song);
+        state.isPlay = true;
+        state.currentIndex += 1;
     }
 }
 
