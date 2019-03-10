837c7167a0a18faac6873d13e5a6758b93e4879b
G-shitou <Gshitou@126.com>
2019-03-10 20:52:41 +0800

    底部播放组件初步初始化

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -4,11 +4,16 @@
             <img :src="singing.img" alt="">        
         </div>
         <div class="lyric">
-            <div class="title"></div>
+            <div class="title">{{singing.title}}</div>
+            <div class="text">
+                <div class="content" ref="lyrics">
+                    <!-- <p v-for="(lyric,index) in singing.lyric" :key="index">{{lyric.text}}</p>     -->
+                </div>
+            </div>
         </div>
         <div class="iconfont" :class="isPlay?'icon-bofang':'icon-bofang1'"></div>
         <div class="iconfont icon-liebiao"></div>
-        <!-- <audio :src="singing.audioSrc" autoplay></audio> -->
+        <audio ref="audio" :src="singing.audioSrc" autoplay @timeupdate="changeLyric()"></audio>
     </div>
 </template>
 
@@ -32,6 +37,16 @@ export default {
   mounted (){
   },
   methods:{
+      changeLyric(){
+          let currentTime = this.$refs.audio.currentTime * 1000;
+          let lyrics = this.singing.lyric;
+          for(let i =0;i<lyrics.length;i++){
+              if(lyrics[i].time >= currentTime+300){
+                  this.$refs.lyrics.innerHTML = (lyrics[i-1]) ? lyrics[i-1].text : '';
+                  break;
+              }
+          }
+      }
   }
 
 }
@@ -41,7 +56,7 @@ export default {
 .player
     width:100%
     height:1rem
-    box-shadow: 0px -0.5px 0 0 $grayColor
+    box-shadow: 0px -1px 0 0 $mainColor
     position:fixed
     bottom:0
     left:0
@@ -51,15 +66,16 @@ export default {
     .logo,.iconfont
         width:.7rem
         height:.7rem
-        margin:0 .2rem
     .logo
         border-radius:.35rem
         overflow:hidden
+        margin:0 .2rem        
         img
             width:100%
             height:100%
     .iconfont
         color:$mainColor
+        margin-right:.2rem
         font-size:.5rem
         display:-webkit-box
         -webkit-box-align:center
@@ -68,4 +84,24 @@ export default {
         -webkit-box-flex:1
         -moz-box-flex:1
         height:.7rem
+        display:-webkit-box
+        -webkit-box-orient:vertical
+        overflow:hidden
+        .title
+            font-size:.3rem
+            line-height:.4rem
+            white-space: nowrap
+            text-overflow: ellipsis
+            overflow: hidden
+            color:$mainColor
+        .text
+            overflow:hidden
+            height:.31rem
+            .content
+                font-size:.24rem
+                line-height:.31rem
+                white-space: nowrap
+                text-overflow: ellipsis
+                overflow: hidden
+                color:$grayColor
 </style>
