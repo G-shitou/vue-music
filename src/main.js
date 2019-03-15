c9ef573349a5394418ac8ac624faf03d1b010b9d
G-shitou <Gshitou@126.com>
2019-03-15 11:29:36 +0800

    引入vue-lazyload

--- src/main.js
+++ src/main.js
@@ -8,10 +8,15 @@ import 'mint-ui/lib/style.css'
 import './assets/css/base.styl'
 import axios from 'axios'
 import store from './store'
+import vueLazyLoad from 'vue-lazyload'
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
 Vue.use(Mint)
+Vue.use(vueLazyLoad,{
+  error:'./assets/img/logo.png',
+  loading:'./assets/img/logo.png'
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
